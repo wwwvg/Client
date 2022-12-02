4936af7ed7c35649be31095f10a0da6d0612073c
Vasily <72005072+wwwvg@users.noreply.github.com>
2022-12-02 11:01:35 +0300

    Подсчет оставшихся байтов с сообщением об ошибке

--- Client/ViewModels/ContentViewModel.cs
+++ Client/ViewModels/ContentViewModel.cs
@@ -7,37 +7,74 @@ namespace Client.ViewModels
 {
     public class ContentViewModel : BindableBase
     {
-        private byte _bytesLeft = 255;
+        public const int MaxBytes = 256;
         public ContentViewModel()
         {
-            TrashBytesAmount1.AddRange(Enumerable.Range(1, 255));
-            TrashBytesAmount2.AddRange(Enumerable.Range(1, 255));
-            DataAmount.AddRange(Enumerable.Range(1, 255));
+            Bytes.AddRange(Enumerable.Range(0, MaxBytes));
+
         }
 
         #region Properties
-        private ObservableCollection<int> _trashBytesAmount1 = new();
-        public ObservableCollection<int> TrashBytesAmount1 { get => _trashBytesAmount1; set => SetProperty(ref _trashBytesAmount1, value); }
+        private ObservableCollection<int> _bytes = new();
+        public ObservableCollection<int> Bytes { get => _bytes; set => SetProperty(ref _bytes, value); }
 
-        private ObservableCollection<int> _trashBytesAmount2 = new();
-        public ObservableCollection<int> TrashBytesAmount2 { get => _trashBytesAmount2; set => SetProperty(ref _trashBytesAmount2, value); }
+        private string _freeBytes;
+        public string FreeBytes { get => _freeBytes; set => SetProperty(ref _freeBytes, value); }
 
-        private ObservableCollection<int> _dataAmount = new();
-        public ObservableCollection<int> DataAmount { get => _dataAmount; set => SetProperty(ref _dataAmount, value); }
+
+        private int _selectedIndexTrash1;
+        public int SelectedIndexTrash1 { get => _selectedIndexTrash1; set => SetProperty(ref _selectedIndexTrash1, value); }
+
+        private int _selectedIndexTrash2;
+        public int SelectedIndexTrash2 { get => _selectedIndexTrash2; set => SetProperty(ref _selectedIndexTrash2, value); }
+
+        private int _selectedIndexData;
+        public int SelectedIndexData { get => _selectedIndexData; set => SetProperty(ref _selectedIndexData, value); }
         #endregion
 
         #region Commands
-        private DelegateCommand _trashComboBoxOpenned1;
-        public DelegateCommand TrashComboBoxOpenned1 => _trashComboBoxOpenned1 ?? (_trashComboBoxOpenned1 = new DelegateCommand(OnTrashComboBoxOpenned1));
+        private DelegateCommand _trashComboBoxSelected1; // ВЫБОР ЭЛЕМЕНТА В КОМБОБОКСЕ 1
+        public DelegateCommand TrashComboBoxSelected1 => _trashComboBoxSelected1 ?? (_trashComboBoxSelected1 = new DelegateCommand(OnTrashComboBoxSelected1));
 
-        public void OnTrashComboBoxOpenned1()
+        public void OnTrashComboBoxSelected1()
         {
-            
+            SetFreeBytesText();
         }
+
+        private DelegateCommand _trashComboBoxSelected2; // ВЫБОР ЭЛЕМЕНТА В КОМБОБОКСЕ 2
+        public DelegateCommand TrashComboBoxSelected2 => _trashComboBoxSelected2 ?? (_trashComboBoxSelected2 = new DelegateCommand(OnTrashComboBoxSelected2));
+
+        public void OnTrashComboBoxSelected2()
+        {
+            SetFreeBytesText();
+        }
+
+        private DelegateCommand _dataComboBoxSelected; // ВЫБОР ЭЛЕМЕНТА В КОМБОБОКСЕ 2
+        public DelegateCommand DataComboBoxSelected => _dataComboBoxSelected ?? (_dataComboBoxSelected = new DelegateCommand(OnDataComboBoxSelected));
+
+        public void OnDataComboBoxSelected()
+        {
+            SetFreeBytesText();
+        }
+
+        
         #endregion
 
         #region Methods
 
+        private int FreeBytesAmount()
+        {
+            return MaxBytes - Bytes[SelectedIndexTrash1] - Bytes[SelectedIndexTrash2] - Bytes[SelectedIndexData] -1;
+        }
+
+        private void SetFreeBytesText()
+        {
+            int amount = FreeBytesAmount();
+            if(amount < 0)
+                FreeBytes = $"Ошибка! Количество оставшихся байтов: {amount}";
+            else
+                FreeBytes = $"Количество оставшихся байтов: {amount}";
+        }
         #endregion
 
         #region Event Handlers
