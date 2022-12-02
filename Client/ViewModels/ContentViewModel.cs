4bee3faf82e42c42f68dba7dd792e12646faf5a0
Vasily <72005072+wwwvg@users.noreply.github.com>
2022-12-02 09:36:22 +0300

    Add project files.

---
+++ Client/ViewModels/ContentViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Prism.Mvvm;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Prism.Commands;
+
+namespace Client.ViewModels
+{
+    public class ContentViewModel : BindableBase
+    {
+        private byte _bytesLeft = 255;
+        public ContentViewModel()
+        {
+            TrashBytesAmount1.AddRange(Enumerable.Range(1, 255));
+            TrashBytesAmount2.AddRange(Enumerable.Range(1, 255));
+            DataAmount.AddRange(Enumerable.Range(1, 255));
+        }
+
+        #region Properties
+        private ObservableCollection<int> _trashBytesAmount1 = new();
+        public ObservableCollection<int> TrashBytesAmount1 { get => _trashBytesAmount1; set => SetProperty(ref _trashBytesAmount1, value); }
+
+        private ObservableCollection<int> _trashBytesAmount2 = new();
+        public ObservableCollection<int> TrashBytesAmount2 { get => _trashBytesAmount2; set => SetProperty(ref _trashBytesAmount2, value); }
+
+        private ObservableCollection<int> _dataAmount = new();
+        public ObservableCollection<int> DataAmount { get => _dataAmount; set => SetProperty(ref _dataAmount, value); }
+        #endregion
+
+        #region Commands
+        private DelegateCommand _trashComboBoxOpenned1;
+        public DelegateCommand TrashComboBoxOpenned1 => _trashComboBoxOpenned1 ?? (_trashComboBoxOpenned1 = new DelegateCommand(OnTrashComboBoxOpenned1));
+
+        public void OnTrashComboBoxOpenned1()
+        {
+            
+        }
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+        #region Event Handlers
+
+        #endregion  
+    }
+}
---
+++ Client/Views/ContentView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Client.Views
+{
+    public partial class ContentView : UserControl
+    {
+        public ContentView()
+        {
+            InitializeComponent();
+        }
+    }
+}
